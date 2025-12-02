c35713acd609e867629517012fcb3e160be05e1d
suryap60 <suryap6061@gmail.com>
2025-12-02 17:57:45 +0530

    ....

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,17 @@ export default function Home() {
         <Navbar navigationItems={navigationItems} />
 
         {/* Hero Section (aligned at bottom) */}
-        <section className="absolute bottom-0 left-0 z-10 flex flex-col justify-end container mx-auto px-6 sm:px-10 md:px-16 lg:px-24 xl:px-32 pb-10 sm:pb-16 md:pb-24 lg:pb-32">
+<section
+  className="
+    absolute inset-0 z-10
+    flex flex-col
+    justify-center sm:justify-end
+    container mx-auto
+    px-6 sm:px-10 md:px-16 lg:px-24 xl:px-32
+    pb-0 sm:pb-16 md:pb-24 lg:pb-32
+  "
+>
+
           <h2 className="font-semibold text-white text-2xl sm:text-3xl md:text-4xl">
             WEBSITE
           </h2>
@@ -81,13 +91,6 @@ export default function Home() {
               },
             ]}
           />
-
-          {/* <p className="[font-family:'Montserrat',Helvetica] text-lg text-gray-200 max-w-3xl">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&#39;s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book.
-          </p> */}
         </section>
       </div>
       <DiscoverSection/>
